e7acdffa78918b0471c6759a2d3ab0798619bcf0
blacy3388 <laszlo.buzas@ou.ac.uk>
2025-07-30 22:19:41 +0100

    Add files via upload

--- secrets.h
+++ secrets.h
@@ -1,12 +1,8 @@
-//#ifndef SECRETS_H
-#define SECRETS_H
-
 #define WIFI_SSID "TP-Link_702F5g"
 #define WIFI_PASSWORD "13558451"
-
-#define MQTT_SERVER "192.168.0.109"
+#define MQTT_SERVER "192.168.0.109"  // Your MQTT broker IP
 #define MQTT_PORT 1883
 #define MQTT_USER "mqtt_laz"
-#define MQTT_PASS "lacyy52299"
-
-//#endif
+#define MQTT_PASSWORD "lacyy52299"
+#define MQTT_TOPIC_PUB "rfid/status"
+#define MQTT_TOPIC_SERVO "servo/control"
