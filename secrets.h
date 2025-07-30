8a5582052844d5dc278216574564ec6a60e1655f
blacy3388 <laszlo.buzas@ou.ac.uk>
2025-07-30 22:25:13 +0100

    Delete secrets.h

--- secrets.h
+++
@@ -1,8 +0,0 @@
-#define WIFI_SSID "TP-Link_702F5g"
-#define WIFI_PASSWORD "13558451"
-#define MQTT_SERVER "192.168.0.109"  // Your MQTT broker IP
-#define MQTT_PORT 1883
-#define MQTT_USER "mqtt_laz"
-#define MQTT_PASSWORD "lacyy52299"
-#define MQTT_TOPIC_PUB "rfid/status"
-#define MQTT_TOPIC_SERVO "servo/control"
